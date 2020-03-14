0401cf3f2a66719f3d37bc1e06f15279f843844d
David Wang <devtech324@gmail.com>
2020-03-15 00:56:07 +0200

    ku

--- backend/src/shared/enums/response-text.enum.js
+++ backend/src/shared/enums/response-text.enum.js
@@ -2,6 +2,6 @@ exports.responseText = {
     'payload-required': { message: 'Payload is required!' },
     'password-wrong': { message: 'Password is wrong!'},
     'email-exist': { message: 'Email is already exist!'},
-    'auth-error': { message: 'Authorization error, try again later.'},
+    'auth-error': { message: 'Authorization failure.'},
     'user-not-found': { message: 'User was not found.'},
 };
